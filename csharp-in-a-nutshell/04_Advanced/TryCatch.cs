2bcf012dd25cf6ae43d8c1fb6708152fb3cf2d70
roderknight <dealarcmarcel99@gmail.com>
2022-12-29 21:58:30 -0400

    Added try/catch statements subchapter

---
+++ csharp-in-a-nutshell/04_Advanced/TryCatch.cs
@@ -0,0 +1,65 @@
+﻿namespace csharp_in_a_nutshell._04_Advanced
+{
+    internal class TryCatch
+    {
+    }
+
+    public class TestTryCatch
+    {
+        public static void Test(string[] args)
+        {
+            try
+            {
+                byte b = byte.Parse(args[0]);
+                Console.WriteLine(b);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Please provide at least one argument");
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex) when (ex.Message == "Input string was not in a correct format.")
+            {
+                //-- this is a exception filter, will hit here if the statement matches
+                Console.WriteLine("That's not a number!");
+                Console.WriteLine(ex.Message);
+            }
+            catch (OverflowException) // we don't ask for the parameter since we're not gonna use it
+            {
+                Console.WriteLine("You've given me more than a byte!");
+            }
+            finally
+            {
+                //-- finally will always hit
+                Console.WriteLine("Wish you luck the next time");
+            }
+        }
+
+        public static void ReadFile()
+        {
+            StreamReader reader = null!;
+
+            try
+            {
+                // if a exception happens this could be easily avoided using the using() statement
+                reader = File.OpenText("../../file.txt"); // you can change the path to force a exception
+                if (reader.EndOfStream)
+                    return;
+                Console.WriteLine(reader.ReadToEnd());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // need to close the reader whatever it happens
+                if (reader != null)
+                    reader.Dispose();
+            }
+        }
+
+        
+    }
+
+}
--- csharp-in-a-nutshell/Program.cs
+++ csharp-in-a-nutshell/Program.cs
@@ -1,31 +1,3 @@
 ﻿using csharp_in_a_nutshell._04_Advanced;
 
 
-
-Action[] actions = new Action[3];
-Action[] actions2 = new Action[3];
-
-//-- each closure captures the same variable
-//-- when the delegates are invoked, each delegate sees the i's value
-//-- at the time of invocation (3)
-for (int i = 0; i < 3; i++)
-{
-    actions[i] = () => Console.WriteLine(i);
-}
-
-foreach (Action action in actions)
-{
-    action();
-}
-
-//-- the solution to this is by using a scoped variable
-for (int i = 0; i < 3; i++)
-{
-    int scopedI = i;
-    actions2[i] = () => Console.WriteLine(scopedI);
-}
-
-foreach (Action action1 in actions2)
-{
-    action1();
-}
