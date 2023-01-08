cafa517217b19a97b478bdd9fc3530e24f4deed9
roderknight <dealarcmarcel99@gmail.com>
2023-01-08 16:03:22 -0400

    Added Attributes subchapters

---
+++ csharp-in-a-nutshell/04_Advanced/Attributes.cs
@@ -0,0 +1,81 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace csharp_in_a_nutshell._04_Advanced
+{
+    internal class Attributes
+    {
+    }
+
+    public class AttributesPlayground
+    {
+        public static void Play() 
+        {
+            
+        }
+    }
+
+    /// <summary>
+    /// Example of a class can have many attributes
+    /// </summary>
+    [Serializable, Obsolete, CLSCompliant(false)]
+    public class Bar
+    { 
+    }
+
+    public class AttrTest
+    {
+        public static void Foo(
+            [CallerMemberName] string memberName = null,
+            [CallerFilePath] string filePath = null,
+            [CallerLineNumber] int lineNumber = 0
+            )
+        {
+            Console.WriteLine(memberName);
+            Console.WriteLine(filePath);
+            Console.WriteLine(lineNumber);
+        }
+    }
+
+    public interface INotifyPropertyChanged
+    {
+        event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public delegate void PropertyChangedEventHandler(object sender, PropertyChangedEventArgs e);
+
+    public class PropertyChangedEventArgs : EventArgs
+    {
+        public virtual string PropertyName { get; }
+        
+        public PropertyChangedEventArgs(string propertyName)
+        {
+            PropertyName = propertyName;
+        }
+    }
+
+    public class Boo : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged =  delegate { };
+
+        void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        string customerName;
+
+        public string CustomerName
+        { 
+            get { return customerName; }
+            set
+            {
+                if (value == customerName) return;
+                customerName = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+
+
+}
--- csharp-in-a-nutshell/Program.cs
+++ csharp-in-a-nutshell/Program.cs
@@ -1,2 +1,5 @@
 ﻿using csharp_in_a_nutshell._04_Advanced;
 
+Boo boo = new Boo();
+boo.CustomerName = "James";
+boo.CustomerName = "Jaime";
